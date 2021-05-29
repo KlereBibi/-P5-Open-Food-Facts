a9e0150f86f8dff762f9a92dac3305acccb27b32
Claire <biedermann.claire@gmail.com>
2021-05-29 18:10:09 +0200

    enregistrement des categories

--- entities/categorymanager.py
+++ entities/categorymanager.py
@@ -1,1 +1,27 @@
+import mysql.connector
+
 class CategoryManager:
+
+
+    def save_into_table(self, element):
+
+        #connexion au base de données
+        db = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "184300",
+            database = "off"
+        )
+        #créer un curseur de base de données pour effectuer des opérations SQL
+        cur = db.cursor()
+
+        #requéte SQL
+        sql = "INSERT INTO categories (id, name) VALUES (%s, %s)"
+        #les valeurs de la requéte SQL
+        value = (None, element)
+        #exécuter le curseur avec la méthode execute() et transmis la requéte SQL
+        cur.execute(sql, value)
+        #valider la transaction
+        db.commit()
+        #afficher le nombre de lignes insérées
+        print(cur.rowcount, "ligne insérée.")
--- entities/productmanager.py
+++ entities/productmanager.py
@@ -1,17 +1,22 @@
 from category import Category
-#from categorymanager import CategoryManager
+from categorymanager import CategoryManager
 
 class ProductManager:
 
     def take_product_list(self, list):
 
+        """méthode permettant de récupérer la liste de produit appeler dans API manager, retournant la liste de produit pour Product Manager """
         return list
     
     def data_cate(self, list):
+
+        """méthode permettant d'accéder aux objets catégories de chaque produits pour ensuite appeler category manager qui enregistrera les produits dans la table"""
+
         for element in self.take_product_list(list):
             for each in element.categories:
                 if each.id == None:
-                    print("ok")
+                    categomana = CategoryManager()
+                    categomana.save_into_table(each.name)
 
         
 # Hey, tu ma file une liste de prodiuits a enregistrer c'est cool :) par contre ces produits contiennent des categories mais pas d'ID ca veut dire qu'ils sont tout neuf
