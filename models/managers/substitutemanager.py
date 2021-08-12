62036bbd29f3b0b79fcdd3dcb38656ba44d7f890
Claire <biedermann.claire@gmail.com>
2021-08-12 23:56:14 +0200

    code fonctionnel - à mettre en forme

--- models/managers/substitutemanager.py
+++ models/managers/substitutemanager.py
@@ -1,21 +1,33 @@
 from models.entities.substitute import Substitute
 from models.managers.manager import Manager
 from models.entities.product import Product
-from models.entities.substitute import Substitute
+from models.entities.store import Store
+from models.entities.brand import Brand
 
 class SubstituteManager(Manager):
 
     def search_substitut(self, product):
 
-        self.cursor.execute("SELECT ok.id, ok.name, ok.nutriscore, ok.url, b.name, s.name  FROM (SELECT DISTINCT p.id, p.name, p.nutriscore, p.url, count(*) as nb FROM products as p INNER JOIN categories_products as cp ON p.id = cp.id_products WHERE cp.id_categories IN (SELECT cp.id_categories FROM categories_products as cp WHERE id_products = %(id_products)s) AND NOT cp.id_products = %(id_products)s GROUP BY cp.id_products ORDER by nutriscore ASC) as ok INNER JOIN brands_products as bp ON ok.id = bp.id_products INNER JOIN stores_products as sp ON ok.id = sp.id_products INNER JOIN brands as b ON b.id = bp.id_brands INNER JOIN stores as s ON s.id = sp.id_stores WHERE nb = (SELECT MAX(nb) FROM (SELECT DISTINCT p.id, p.name, p.nutriscore, p.url, count(*) as nb FROM products as p INNER JOIN categories_products as cp ON p.id = cp.id_products WHERE cp.id_categories IN (SELECT cp.id_categories FROM categories_products as cp WHERE id_products = %(id_products)s) AND NOT cp.id_products = %(id_products)s GROUP BY cp.id_products ORDER by nutriscore ASC) as o) AND ok.nutriscore < %(nutriscore)s LIMIT 1", {'id_products' : product.id, 'nutriscore': product.nutriscore})
+        self.cursor.execute("SELECT ok.id, ok.name, ok.nutriscore, ok.url, b.name, s.name  FROM (SELECT DISTINCT p.id, p.name, p.nutriscore, p.url, count(*) as nb FROM products as p INNER JOIN categories_products as cp ON p.id = cp.id_products WHERE cp.id_categories IN (SELECT cp.id_categories FROM categories_products as cp WHERE id_products = %(id_products)s) AND NOT cp.id_products = %(id_products)s GROUP BY cp.id_products ORDER by nutriscore ASC) as ok INNER JOIN brands_products as bp ON ok.id = bp.id_products INNER JOIN stores_products as sp ON ok.id = sp.id_products INNER JOIN brands as b ON b.id = bp.id_brands INNER JOIN stores as s ON s.id = sp.id_stores WHERE nb = (SELECT MAX(nb) FROM (SELECT DISTINCT p.id, p.name, p.nutriscore, p.url, count(*) as nb FROM products as p INNER JOIN categories_products as cp ON p.id = cp.id_products WHERE cp.id_categories IN (SELECT cp.id_categories FROM categories_products as cp WHERE id_products = %(id_products)s) AND NOT cp.id_products = %(id_products)s GROUP BY cp.id_products ORDER by nutriscore ASC) as o) AND ok.nutriscore < %(nutriscore)s AND ok.id = (SELECT lo.id FROM (SELECT DISTINCT p.id, count(*) as nb FROM products as p INNER JOIN categories_products as cp ON p.id = cp.id_products WHERE cp.id_categories IN ( SELECT cp.id_categories FROM categories_products as cp WHERE id_products = 1134) AND NOT cp.id_products = 1134 GROUP BY cp.id_products ORDER by nutriscore ASC) as lo WHERE nb = (SELECT MAX(nb) FROM (SELECT DISTINCT p.id, count(*) as nb FROM products as p INNER JOIN categories_products as cp ON p.id = cp.id_products  WHERE cp.id_categories IN (SELECT cp.id_categories FROM categories_products as cp WHERE id_products = 1134 ) AND NOT cp.id_products = 1134 GROUP BY cp.id_products ORDER by nutriscore  ASC) as o ) AND ok.nutriscore <  'd' LIMIT 1);", {'id_products' : product.id, 'nutriscore': product.nutriscore})
 
         substitut = self.cursor.fetchall()
 
+        
+
         if substitut:
-            
+            product_substitute = None
             for element in substitut:
-                product_substitute = Product(element[1], element[2], None, element[3], None, None, element[0])
-                return product_substitute
+                if product_substitute == None:
+                    product_substitute = Product(element[1], element[2], None, element[3], [Brand(element[4])], [Store(element[5])], element[0])   
+                else:
+                    for brand in product_substitute.brands:
+                        if brand.name != element[4]:
+                            product_substitute.brands.append(Brand(element[4]))
+                    for store in product_substitute.stores:
+                        if store.name != element[5]:
+                            product_substitute.stores.append(Store(element[5]))
+                        
+            return product_substitute
         else:
             return False
 
